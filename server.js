50c7aa48e9bf9d2c80af9489441044e0029b2d2a
unknown <duongnguyencam05@gmail.com>
2023-10-21 18:24:12 +0700

    next up

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const app = express();
 // PORT
 const port = process.env.PORT
 // config cors 
+
 const corsOptions = {
-     origin: 'https://haweb-admin.vercel.app', // Replace with your frontend URL
+     origin: 'https://haweb-admin.vercel.app/', // Replace with your frontend URL
      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
      credentials: true, // Required for cookies, if you're using them
      optionsSuccessStatus: 204,
    };
 app.use((req, res, next) => {
-     res.header('Access-Control-Allow-Origin', 'https://haweb-admin.vercel.app');
+     res.header('Access-Control-Allow-Origin','https://haweb-admin.vercel.app/');
      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
      res.header('Access-Control-Allow-Headers', 'Content-Type');
      next();
